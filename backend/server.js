c26f5c49eb05deb01df90680de38b7a77ee34a6c
muharremgem <muharremgem28@gmail.com>
2024-01-29 17:21:46 +0300

    mongodb connect

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,29 @@
-const a = 5;
+const express = require("express");
+const mongoose = require("mongoose");
+const port = 3000;
 
-console.log(a);
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://muharremgem:GplaNahsNwANXV5f@mern-e-commerce.qjzlzxt.mongodb.net/"
+    );
+    console.log("Connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Nodemon Express.js");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Bu api");
+});
+
+app.listen(3000, () => {
+  connect();
+  console.log("Server started on port 3000");
+});
