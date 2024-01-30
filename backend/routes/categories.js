6dcc818a95c144a35b73229701322f58d9a0d05b
muharremgem <muharremgem28@gmail.com>
2024-01-30 13:52:26 +0300

    postman

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const myData = req.body;
+
+    console.log(myData);
+    res.status(200).send("ok");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // Tüm kategoriler  getirme (Read- All)
 router.get("/", async (req, res) => {
   res.send("Kategoriler getirildi!");
 });
-
-
-
-
-
 
 module.exports = router;
